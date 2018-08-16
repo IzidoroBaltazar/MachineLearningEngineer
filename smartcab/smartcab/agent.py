edef8fcbd8d040b3f63517fa3817e2b87c9e3eee
Martin Piják <mpijak@outlook.com>
2018-08-16 21:03:04 +0200

    [FEATURE] smartcab project investigation

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -101,7 +101,7 @@ class LearningAgent(Agent):
         # Set the agent state and default action
         self.state = state
         self.next_waypoint = self.planner.next_waypoint()
-        action = None
+        action = random.choice(self.valid_actions)
 
         ###########
         ## TO DO ##
@@ -159,7 +159,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent, epsilon=10, alpha=0.8)
+    agent = env.create_agent(LearningAgent, epsilon=10, alpha=0.8, learning=False)
 
     ##############
     # Follow the driving agent
@@ -174,14 +174,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env)
+    sim = Simulator(env, update_delay=0.01, log_metrics=True, display=False)
 
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run()
+    sim.run(n_test=10)
 
 
 if __name__ == '__main__':
